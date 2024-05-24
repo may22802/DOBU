78b5cca825147becfe68be21b0b28135409f6c5d
may22802 <maymyatmon22802@gmail.com>
2024-05-24 23:10:33 +0630

    private class page

---
+++ JS/privateClass.js
@@ -0,0 +1,24 @@
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        } else {
+          event.preventDefault(); // Prevent default form submission
+  
+          // Hide the form
+          const myModal = new bootstrap.Modal(document.getElementById('successModal'));
+          myModal.show();
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  
+  })()
