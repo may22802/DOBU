7468c4dcf1fa80154f91022e4cfcd40e795ecad6
may22802 <maymyatmon22802@gmail.com>
2024-05-24 22:09:58 +0630

    martial art page

--- JS/contact.js
+++ JS/contact.js
@@ -1,27 +1,27 @@
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }else {
-            event.preventDefault(); // Prevent default form submission
+  'use strict'
 
-            // Hide the form
-            document.getElementById('before').classList.add('d-none');
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-            // Show the content div
-            document.getElementById('after').classList.remove('d-none');
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      } else {
+        event.preventDefault(); // Prevent default form submission
 
-  })()
+        // Hide the form
+        document.getElementById('before').classList.add('d-none');
+
+        // Show the content div
+        document.getElementById('after').classList.remove('d-none');
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+
+})()
 
--- JS/membership.js
+++ JS/membership.js
@@ -1,73 +1,73 @@
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }else {
-            event.preventDefault(); // Prevent default form submission
+  'use strict'
 
-            // Hide the form
-            const myModal = new bootstrap.Modal(document.getElementById('successModal'));
-            myModal.show();
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
-  })()
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      } else {
+        event.preventDefault(); // Prevent default form submission
 
-let maximumCheckBoxCount=0;
+        // Hide the form
+        const myModal = new bootstrap.Modal(document.getElementById('successModal'));
+        myModal.show();
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
 
-function selectedPlan(planID,price,maximumCheck){
-    let plans = document.querySelectorAll('.plan');
-    plans.forEach(function(plan){
-        plan.classList.remove('selected');
-    })
+})()
 
-    let selectedPlan = document.getElementById(planID);
-    selectedPlan.classList.add('selected');
+let maximumCheckBoxCount = 0;
 
-    let summary = document.getElementById('summary');
-    summary.innerHTML = ": $" + price +".00";
+function selectedPlan(planID, price, maximumCheck) {
+  let plans = document.querySelectorAll('.plan');
+  plans.forEach(function (plan) {
+    plan.classList.remove('selected');
+  })
 
-    let total = document.getElementById('total');
-    total.innerHTML = ": $" + price +".00";
+  let selectedPlan = document.getElementById(planID);
+  selectedPlan.classList.add('selected');
 
-     // Enable checkboxes based on the selected plan
-     var checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
-     checkboxes.forEach(function(checkbox) {
-        checkbox.disabled = false;
-      checkbox.checked = false;
-     });
-     maximumCheckBoxCount = maximumCheck;
+  let summary = document.getElementById('summary');
+  summary.innerHTML = ": $" + price + ".00";
+
+  let total = document.getElementById('total');
+  total.innerHTML = ": $" + price + ".00";
+
+  // Enable checkboxes based on the selected plan
+  var checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
+  checkboxes.forEach(function (checkbox) {
+    checkbox.disabled = false;
+    checkbox.checked = false;
+  });
+  maximumCheckBoxCount = maximumCheck;
 }
 
 function toggleCheckbox() {
-    var checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
-    var checkedCount = 0;
+  var checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
+  var checkedCount = 0;
 
-    // Count how many checkboxes are checked
-    checkboxes.forEach(function(checkbox) {
-      if (checkbox.checked) {
-        checkedCount++;
-      }
-    });
+  // Count how many checkboxes are checked
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      checkedCount++;
+    }
+  });
 
-    // Disable/enable checkboxes based on the checked count and maximumCheckBoxCount
-    checkboxes.forEach(function(checkbox) {
-      if (checkedCount >= maximumCheckBoxCount && !checkbox.checked) {
-        checkbox.disabled = true;
-      } else {
-        checkbox.disabled = false;
-      }
-    });
+  // Disable/enable checkboxes based on the checked count and maximumCheckBoxCount
+  checkboxes.forEach(function (checkbox) {
+    if (checkedCount >= maximumCheckBoxCount && !checkbox.checked) {
+      checkbox.disabled = true;
+    } else {
+      checkbox.disabled = false;
+    }
+  });
 }
 
 
